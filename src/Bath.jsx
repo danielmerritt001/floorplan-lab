329a3395fd93b922c52225bb4f3e878d86536126
Daniel Merritt <daniel.mafou@gmail.com>
2023-05-01 20:44:04 -0400

    Change id for bath divs to lower case

--- src/Bath.jsx
+++ src/Bath.jsx
@@ -1,7 +1,7 @@
 const Bath = (props) => {
   return (  
     //Is it a problem that the ide of this is capitalized? As in, "#Full" and "#Half"
-    <div id={props.size}>{props.size} Bath</div>
+    <div id={props.size.toLowerCase()}>{props.size} Bath</div>
   );
 }
 
